4bc0fc0ee7cac3fb71bf17db8709adc2e1b63140
TheAdrianVera <agvm94@gmail.com>
2024-12-02 23:55:58 -0600

    added my fonts to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+              className="text-sm duration-500 text-kanit-500 hover:text-kanit-300"
             >
               {item.name}
             </Link>
