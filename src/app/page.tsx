67b391621ac3bb8c280fd4827f6abebb062fc839
TheAdrianVera <agvm94@gmail.com>
2024-12-03 00:11:04 -0600

    added my logo to the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,9 @@ const navigation = [
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden">
+      <div className='Logo'>
+        <img className='w-16' src='/logos/adriangvera-com-white.png' alt='Adriangvera.com Logo'/>
+      </div>
       <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
@@ -32,7 +35,6 @@ export default function Home() {
         Adrian Vera
       </h1>
 
-      {/* <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" /> */}
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
           Chicago Based Software Engineer
