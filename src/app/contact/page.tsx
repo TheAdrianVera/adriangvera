9617ebbea00d305eca9f5c8eadaa95dd94e20cec
TheAdrianVera <agvm94@gmail.com>
2024-12-14 17:18:17 -0600

    added project & contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Contact() {
+
+    return (
+        <div className='relative pb-16'>
+            <h1>Contact Page</h1>
+        </div>
+    )
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Projects() {
+    return (
+        <div className="relative pb-16">
+            PROJECTS
+        </div>
+    )
+}
