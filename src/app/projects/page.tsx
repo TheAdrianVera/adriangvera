8ef3dad0389d784c904026302eec5e19a7c662ea
TheAdrianVera <agvm94@gmail.com>
2025-02-16 16:29:45 -0600

    updated project page colors

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Projects() {
     return (
-        <div className='bg-mycolors-white relative w-screen h-screen flex justify-center items-center'>
+        <div className='bg-mycolors-yellow relative w-screen h-screen flex justify-center items-center'>
             <h1 className='flex justify-center text-[2rem] text-mycolors-black'>Projects</h1>
         </div>
     )
