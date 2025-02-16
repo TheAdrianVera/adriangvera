08d3c59b932d6bd90d5f4ebe0edb4ff459a15734
TheAdrianVera <agvm94@gmail.com>
2025-02-16 16:26:04 -0600

    added project page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Projects() {
+    return (
+        <div className='bg-mycolors-white relative w-screen h-screen flex justify-center items-center'>
+            <h1 className='flex justify-center text-[2rem] text-mycolors-black'>Projects</h1>
+        </div>
+    )
+}
