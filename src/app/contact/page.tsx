45a7f6ca0685aca4a654df9c19f13e93aa789f5b
TheAdrianVera <agvm94@gmail.com>
2025-02-21 16:31:00 -0600

    prelim styling

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,24 @@
 export default function Contact() {
+    const email = "agvm94@gmail.com"
+    const linkedinUrl = "https://www.linkedin.com/in/adrian-vera-6180a7b6/"
+    const githubUrl = "https://github.com/TheAdrianVera"
+
     return (
         <div className='bg-mycolors-periwinkle relative w-screen h-screen flex justify-center items-center'>
-            <h1 className='flex justify-center text-[2rem] text-mycolors-black'>Contact</h1>
+            <div>
+                <h1 className='flex justify-center text-[2rem] text-mycolors-black pb-8'>Contact</h1>
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
+                    <a href={`mailto:`+email} target="_blank" className='p-6 border-black border-2 rounded-lg flex flex-col items-center hover:bg-gray-900 group transition duration-500 ease-in-out transform hover:scale-105 hover:rotate-2'>
+                        <h2 className='text-xl font-semibold text-black group-hover:text-white'>Email</h2>
+                    </a>
+                    <a href={githubUrl} target="_blank" className='p-6 border-black border-2 rounded-lg shadow-lg flex flex-col items-center hover:bg-gray-900 group transition duration-500 ease-in-out transform hover:scale-105 hover:rotate-2'>
+                        <h2 className='text-xl font-semibold text-black group-hover:text-white'>GitHub</h2>
+                    </a>
+                    <a href={linkedinUrl} target="_blank" className='p-6 border-black border-2 rounded-lg shadow-lg flex flex-col items-center hover:bg-gray-900 group transition duration-500 ease-in-out transform hover:scale-105 hover:rotate-2'>
+                        <h2 className='text-xl font-semibold text-black group-hover:text-white'>LinkedIn</h2>
+                    </a>
+                </div>
+            </div>
         </div>
-        
     )
 }
