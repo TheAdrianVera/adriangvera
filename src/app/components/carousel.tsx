c2044079cd8663ceb2a54d04253ff51807d1dd75
TheAdrianVera <agvm94@gmail.com>
2025-03-05 12:07:11 -0600

    carousel placeholder

---
+++ src/app/components/carousel.tsx
@@ -0,0 +1,43 @@
+interface CarouselProps {
+    logoUrls: [
+        {
+            name: 'AWS',
+            url: '../../../logos/client_logos/awslogo.png'
+        },
+        {
+            name: 'Aditude',
+            url: '../../../logos/client_logos/aditudelogo.png'
+        },
+        {
+            name: 'Advanced',
+            url: '../../../logos/client_logos/ahsllclogolong.png'
+        },
+        {
+            name: 'Betfully',
+            url: '../../../logos/client_logos/betfullylogo.png'
+        },
+        {
+            name: 'F.H. Paschen',
+            url: '../../../logos/client_logos/fhplogo.png'
+        },
+        {
+            name: 'PwC',
+            url: '../../../logos/client_logos/pwclogo.png'
+        },
+        {
+            name: 'UIUC',
+            url: '../../../logos/client_logos/uiuclogo.png'
+        },
+    ]
+}
+
+const Carousel:React.FC<CarouselProps> = () => {
+
+    return (
+        <>
+        </>
+    )
+}
+
+
+export default Carousel
