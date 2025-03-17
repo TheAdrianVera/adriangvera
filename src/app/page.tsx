cffdd60ee140e034d497aed3e6d778952a2637e8
TheAdrianVera <agvm94@gmail.com>
2025-03-17 15:36:26 -0500

    fixed unused imports for push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import React from "react"
 import Link from "next/link"
 import Image from "next/image"
-import { motion as m } from "motion/react"
 import Particlez from "./components/Particlez"
 
 
