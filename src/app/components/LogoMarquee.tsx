fe126f3408042b8acabac00403557149566e3ee8
TheAdrianVera <agvm94@gmail.com>
2025-03-25 14:00:02 -0500

    Marquee: Added additional padding

--- src/app/components/LogoMarquee.tsx
+++ src/app/components/LogoMarquee.tsx
@@ -15,13 +15,13 @@ interface CarouselProps {
 const Carousel:React.FC<CarouselProps> = ({logoUrls}) => {
 
     return (
-        <div className='w-full overflow-hidden px-5 md:px-20 lg:px-40 pb-20'>
+        <div className='w-full overflow-hidden px-5 md:px-20 lg:px-40 pb-20 pt-10'>
             <div className='flex justify-center items-center'>
                 <h1 className='text-black text-3xl font-bold'>Experience</h1>
             </div>
             <Marquee 
                 speed={100}
-                className='w-full pt-10'
+                className='w-full pt-20'
             >
                 {logoUrls.map((logo, index)=>(
                 <Link href={logo.website} key={index} target='_blank' >
