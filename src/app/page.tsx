c39f10fe38ef4a4bb928cf4873741ffaf3aecb4e
TheAdrianVera <agvm94@gmail.com>
2025-09-19 23:15:57 -0500

    style: updated homepage font sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           Adrian Vera
         </h1>
         <div className="my-16 text-center animate-fade-in">
-          <h2 className="text-sm text-zinc-500 ">
+          <h2 className="text-lg md:text-xl text-zinc-500 ">
             Chicago Based Software Engineer
           </h2>
         </div>
