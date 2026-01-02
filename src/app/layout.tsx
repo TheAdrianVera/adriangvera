24789d007a71f2828d0a0329c6156744e171493e
TheAdrianVera <agvm94@gmail.com>
2026-01-01 18:20:22 -0600

    bug: white screen fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`antialiased`}
+        className={`antialiased bg-background`}
       >
       <Navbar />
       <AnimateBody>
